8fd1e59ac16ce14557b2e95fb128c3e6af5d5421
Riya Mishra <riamishra622@gmail.com>
2021-01-21 13:50:42 +0530

    second commit

--- build/main.js
+++ build/main.js
@@ -1,6 +1,8 @@
 
   // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optiona
+  
+
   var firebaseConfig = {
     apiKey: "AIzaSyB4t3ulszufSf8XegN2CTgOqGCuB-Rxm1k",
     authDomain: "form-validation-27e33.firebaseapp.com",
@@ -12,51 +14,65 @@
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
 
 
+
+  document.getElementById('form-regi').addEventListener('submit', submitForm);
+
+  //submit form
+  function submitForm(e){
+  // e.preventDefault();
+  
+   var name = getInputValue('username');
+   var emailid= getInputValue('email');
+   var email=emailid.toString();
+   var password = getInputValue('password');
+  }
+  function getInputValue(id){
+    return document.getElementById(id).value;
+  }
+    
+   
+  
 //reference messages collection
 //var messagesRef = firebase.database().ref('messages');
 
 //fireebase auth starts
-// firebase.auth().createUserWithEmailAndPassword(email, password)
-//   .then((user) => {
+firebase.auth().createUserWithEmailAndPassword(submitForm.email, submitForm.password)
+  .then((submitForm) => {
+    // Signed in 
+    console.log('successfully created user id');
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log('error creating the form');
+    // ..
+  });
+
+
+
+//   firebase.auth().signInWithEmailAndPassword(submitForm.emailid, submitForm.password)
+//   .then((submitForm) => {
 //     // Signed in 
 //     // ...
 //   })
 //   .catch((error) => {
 //     var errorCode = error.code;
 //     var errorMessage = error.message;
-//     // ..
 //   });
-//firebase auth ends
 
-document.getElementById('form-regi').addEventListener('submit', submitForm);
 
-//submit form
-function submitForm(e){
- e.preventDefault();
-
- var name = getInputValue('username');
- var emailid= getInputValue('email');
- var password = getInputValue('password');
-
-//save message
-
- saveMessage(username,email,password);
-}
-//function to get form value
-
-function getInputValue(id){
-    return document.getElementById(id).value;
-}
-
-//save message to firebase
- function saveMessage(username,email,password){
-     //var newMessageRef =messagesRef.push();
-     function newMessageRef.set({
-         username: username,
-         email: email,
-         password: password
-     });
- }
+//   firebase.auth().onAuthStateChanged((submitForm) => {
+//     if (submitForm) {
+//       // User is signed in, see docs for a list of available properties
+//       // https://firebase.google.com/docs/reference/js/firebase.User
+//       var uid = submitForm.name;
+//       // ...
+//     } else {
+//       // User is signed out
+//       // ...
+//     }
+//   });
+// s  
